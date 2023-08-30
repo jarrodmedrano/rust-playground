2881b830d38a811d783be2262b1b9cb603329f68
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2023-08-30 09:04:01 -0500

    added built in enum

--- target/debug/deps/playaround-269bcc6a291d6e4d.d
+++
@@ -1,6 +0,0 @@
-/Users/jamedrano/work/playaround/target/debug/deps/playaround-269bcc6a291d6e4d: src/main.rs src/m1_enums.rs
-
-/Users/jamedrano/work/playaround/target/debug/deps/playaround-269bcc6a291d6e4d.d: src/main.rs src/m1_enums.rs
-
-src/main.rs:
-src/m1_enums.rs:
--- target/debug/playaround.d
+++
@@ -1,1 +0,0 @@
-/Users/jamedrano/work/playaround/target/debug/playaround: /Users/jamedrano/work/playaround/src/m1_enums.rs /Users/jamedrano/work/playaround/src/main.rs
