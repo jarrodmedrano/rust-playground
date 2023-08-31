37fbf9844be6fb4df3f7453bf37292da1fd36a59
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2023-08-31 08:28:53 -0500

    added some structs and implementations

--- src/m2_structs.rs
+++ src/m2_structs.rs
@@ -6,18 +6,47 @@ struct User {
     active: bool,
 }
 
+impl User {
+    fn increment_sign_in_count(&mut self) {
+        self.sign_in_count += 1;
+    }
+    fn change_username(&mut self, new_username: &str) {
+        self.username = String::from(new_username);
+    }
+    fn change_email(&mut self, new_email: &str) {
+        self.email = String::from(new_email);
+    }
+}
+
+fn change_username(user: &mut User, new_username: &str) {
+    user.username = String::from(new_username);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_structs() {
-        let user_1 = User {
+        let mut user_1 = User {
             username: String::from("Rust"),
             email: String::from("someone@example.com"),
             sign_in_count: 1,
             active: true,
         };
+
+        change_username(&mut user_1, "new_username");
+
+        let mut user_2: User = User {
+            username: String::from("new_username"),
+            email: String::from("someone@example.com"),
+            sign_in_count: 0,
+            active: false,
+        };
+
+        user_2.increment_sign_in_count();
+
+        user_2.change_email("new_email");
 
         dbg!(user_1);
     }
